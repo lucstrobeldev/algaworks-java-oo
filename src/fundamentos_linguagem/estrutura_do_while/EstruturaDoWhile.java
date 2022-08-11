26283eb50095988e402375fd799033deed070412
Lucas Strobel <lucas.almeida@totemti.com.br>
2022-08-11 16:04:07 -0400

    Aplicando o uso de do-while

---
+++ src/fundamentos_linguagem/estrutura_do_while/EstruturaDoWhile.java
@@ -0,0 +1,19 @@
+package fundamentos_linguagem.estrutura_do_while;
+
+import java.util.Scanner;
+
+public class EstruturaDoWhile {
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+
+        int valor;
+        int soma = 0;
+
+        do {
+            System.out.println("Digite 0 para sair ou qualquer numero para somar: ");
+            valor = entrada.nextInt();
+            soma += valor;
+            System.out.println("Soma: " + soma);
+        } while (valor != 0);
+    }
+}
