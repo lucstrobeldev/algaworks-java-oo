d6806c255ad533728853988eab831ca3651a560c
Lucas Strobel <lucas.almeida@totemti.com.br>
2022-08-15 10:45:52 -0400

    Introducao a Orientacao a Objetos

---
+++ src/poo_programacao_orientada_objetos/IntroducaoOrientadaObjetos.java
@@ -0,0 +1,14 @@
+package poo_programacao_orientada_objetos;
+
+public class IntroducaoOrientadaObjetos {
+
+    // ela é um paradigma de programacao que ajuda a definar a estrutura de programa de computadores e aparelhos eletronicos
+
+    // Baseado nos conceitos do mundo real para serem aplicados nos programas
+
+    // algumas das vantagens ao utilizar a POO, é aumentar a produtividade, reutilizacao de código, reduz linhas de código
+
+    // Programas componentizados
+
+
+}
