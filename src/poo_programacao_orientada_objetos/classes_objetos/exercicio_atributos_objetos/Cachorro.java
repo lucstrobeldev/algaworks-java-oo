33c7917e20ac55db32f5a1c1ab89ce71bec89353
Lucas Strobel <lucas.almeida@totemti.com.br>
2022-08-15 11:51:07 -0400

    Exercicio para praticar a instancia e acesso aos atributos do objeto

---
+++ src/poo_programacao_orientada_objetos/classes_objetos/exercicio_atributos_objetos/Cachorro.java
@@ -0,0 +1,9 @@
+package poo_programacao_orientada_objetos.classes_objetos.exercicio_atributos_objetos;
+
+public class Cachorro {
+
+    String nome;
+    String raca;
+    char sexo;
+    int idade;
+}
