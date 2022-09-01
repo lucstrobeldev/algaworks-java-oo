f90b0498ae04dbc2faa091ed9944ed78d727d6e1
Lucas Strobel <lucas.almeida@totemti.com.br>
2022-09-01 16:33:49 -0400

    Adiciona a definicao de construtor

--- src/poo_v2/construtores/TesteConstrutor.java
+++ src/poo_v2/construtores/TesteConstrutor.java
@@ -10,4 +10,7 @@ public class TesteConstrutor {
         Pessoa p2 = new Pessoa("Marisa", 22);
         System.out.println("Nome: \"" + p2.nome + "\" tem " + p2.idade + " anos.");
     }
+    //Em Java, o construtor é definido como um método cujo nome deve ser o mesmo nome da classe e sem indicação do tipo de retorno
+    // -- nem mesmo void. O construtor é unicamente invocado no momento da criação do objeto através do operador new.
+    // O retorno do operador new é uma referência para o objeto recém-criado.
 }
