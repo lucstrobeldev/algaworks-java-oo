d59697b194a11c2d81ff5a99e1d0b9ba4ba70550
Lucas Strobel <lucas.almeida@totemti.com.br>
2022-09-01 14:59:48 -0400

    Adicionando a definicao do objeto this ao exemplo

--- src/poo_v2/objeto_this/ExemploThis.java
+++ src/poo_v2/objeto_this/ExemploThis.java
@@ -8,6 +8,10 @@ public class ExemploThis {
         carro.modelo = "Palio";
 
         System.out.println("Modelo antes: " + carro.modelo);
+        // O this é usado para referenciar a um atributo externo fora do metodo.
+        // Por exemplo ser dentro de um método se estiver dois atributos com o mesmo nome
+        // oque estiver com a palavra "this" estará
+        // se referenciando a um atributo fora do metodo.
         carro.alterarModelo("Civic");
 
         System.out.println("Modelo depois: " + carro.modelo);
