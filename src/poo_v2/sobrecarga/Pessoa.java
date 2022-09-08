d1f0b5aee52afda6abf390ad0e41b133b8966420
Lucas Strobel <lcs.strobel@gmail.com>
2022-09-08 12:41:36 -0400

    aplicando sobreposicao, utilizando 3 metodos diferentes, criados da mesma maneira mas para finalidades diferentes

--- src/poo_v2/sobrecarga/Pessoa.java
+++ src/poo_v2/sobrecarga/Pessoa.java
@@ -1,4 +1,32 @@
 package poo_v2.sobrecarga;
 
 public class Pessoa {
+
+    private String nome;
+    private int idade;
+
+
+    //construtor para que faz com que o objeto Pessoa exista
+    public Pessoa(String nome, int idade) {
+        this.nome = nome;
+        this.idade = idade;
+    }
+
+    // get para usar a informacao que será registrada dentro da variavel criada no objeto ou do objeto
+    public String getNome() {
+        return nome;
+    }
+
+    // set para poder salvar a informacao dentro da variavel criada no objeto ou do objeto
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
 }
--- src/poo_v2/sobrecarga/TesteCadastro.java
+++ src/poo_v2/sobrecarga/TesteCadastro.java
@@ -2,6 +2,18 @@ package poo_v2.sobrecarga;
 
 public class TesteCadastro {
     public static void main(String[] args) {
+        CadastroPessoa cadastroPessoa = new CadastroPessoa();
+
+        Pessoa pessoa = new Pessoa("José", 32);
+        cadastroPessoa.cadastrar(pessoa);
+
+        //-----------------------------------------------------
+
+        cadastroPessoa.cadastrar("Maria", 24);
+
+        //----------------------------------------------------
+
+        cadastroPessoa.cadastrar("Lucas");
 
     }
 }
