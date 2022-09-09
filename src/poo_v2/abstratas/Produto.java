2e9e283386428bc28d0adcaaf3a029ffb875d6df
Lucas Strobel <lcs.strobel@gmail.com>
2022-09-09 08:46:11 -0400

    entendo as classes abstratas e seus usos

---
+++ src/poo_v2/abstratas/Produto.java
@@ -0,0 +1,10 @@
+package poo_v2.abstratas;
+
+public abstract class Produto {
+
+    //uma classe abstrata é quando um objeto é mais generalizado, nao tao bem definido e portanto
+    //utilizamos o operador abstract
+    //algumas regras para quando fazemos uma classe abstrata
+    //utilizada para a herança
+    //quando temos uma class do tipo abstract nao é possivel criar uma instancia dessa classe
+}
---
+++ src/poo_v2/abstratas/ProdutoPerecivel.java
@@ -0,0 +1,4 @@
+package poo_v2.abstratas;
+
+public class ProdutoPerecivel extends Produto{
+}
